918404e936da9ca51472107521430e2432b39725
rinconrodriguezjr <rinconrodriguezjr@gmail.com>
2023-02-02 20:06:41 -0500

    se agrega funcionalidades carrito

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -120,7 +120,7 @@ const Products = () => {
 
                         <div className='inputAddCart'>
                             <div className="priceCart">
-                                <b style={{ color: "ligthGray" }}>Price</b> <br /> ${products.price}
+                                <b style={{ color: "ligthGray" }}>Price</b> <br /> ${Number(products.price) * quantity}
                             </div>
                             <div className='quantityCart'>
                                 <button onClick={down}><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAT5JREFUSEvtlOExBEEQhb+LgAwQASJABMjgMkAEiAAhiAARIAInAmRABuq76tm6uhuzvar2hyr9Z2t3e97rfv16Jowck5Hx+SfoVXhRohmw3XsilyDWrqljEbwCO8sEfniJAg+Ap1yxXdY+8BhvVm8XKy66AM6B92jxM0myHsVtApeAOPOo2bTM4h44ThKYewg8A3bSRY3AKiRZCwIPt+IIuAO+Qne7bxL48xS4ApRoK541EqV5A3yeAdfLSa1Ndsh7MWyHXouS8wDYyUq0CKzKdpWqVl3pUmmUtWqIvruo6OthrVf0FVBLW4RG+HFOfQS2XBzSbWeYwK2/BaYtB2QIrFLwjfC4eO7KR7imuSsZAgEXt7QUnNr2LIGgWvAk0G/Cyr17OIRAqcr9NL/IMjGEIIM3aA9+BThkk/8GwTcJ2TYZqCLviwAAAABJRU5ErkJggg=="/></button>
